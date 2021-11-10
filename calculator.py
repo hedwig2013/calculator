00f5eb982625b39e693a9d4cd43d03b45cfd98e1
Your Name <you@example.com>
2021-11-10 22:40:02 +0100

    wip

--- calculator.py
+++ calculator.py
@@ -58,13 +58,47 @@ def main():
     print(final_result)
 
 
-def calculate(c, current_number, possible_operations, result):
-    if c not in possible_operations:
+def calculate(operation, current_number, possible_operations, result):
+    """
+    Dies ist der eigentliche Rechner. Es werden 4 Parameter übergeben:
+    operation: Die mathematische Operation +, -, *, /
+    current_number: Die aktuelle Zahl
+    possible_opertions: Ein Dictionary, dass die Operation einer Function zuordnet
+    result: Das aktuelle ergebnis, soweit die Kettenrechnung fortgeschritten ist.
+
+    Der Kniff hier ist, dass wir weder if noch switch benutzen, um anhand
+    der Operation zu entscheiden, welche Function aufgerufen werden muss.
+
+    In dem Dictionary suchen wir einfach nach der Operation und bekommen so
+    die Function zurück, die wir brauchen. Diese können wir direkt aufrufen:
+    ```
+    possible_operations[operation](result, current_number)
+    ```
+    Wollte man das mittels if machen, dann sähe das so aus:
+    ```
+    if operation == '+':
+        r=addition(result,current_number)
+    else if operation == '-':
+        r=subtraction(result,current_number)
+    else if operation == '*':
+        r=product(result,current_number)
+    else if operation == '/':
+        r=division(result,current_number)
+    ```
+
+    Wie Du siehst, wird das Dictionary mit der Zurodnung der Operanden
+    zu den benötigten Funktionen dieser Funktion übergeben. Warum ich
+    das mache? Um Dir zu zeigen, dass man Funktionen wie Daten und Variablen zwischen
+    anderen Funktionen herumreichen kann.
+    """
+    if operation not in possible_operations:
         return result, current_number
+        
     current_number = float(current_number)
-    r = possible_operations[c](result, current_number)
+    r = possible_operations[operation](result, current_number)
     return r, ""
 
-
+# Wenn dieser Code nicht als Modul aufgreufen wird, dann rufe main auf.
+# Ansonsten passiert nichts.
 if (__name__ == "__main__"):
     main()
